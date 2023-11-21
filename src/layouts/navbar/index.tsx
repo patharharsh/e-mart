e7d1f13c726c3facf1aa652dc700c6aa468ab2db
patharharsh <patharharsh@gmail.com>
2023-11-21 11:36:02 +0530

    mui addded navbar updated

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -3,73 +3,154 @@ import React, { useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { TbGardenCart } from "react-icons/tb";
 import { GoHeart } from "react-icons/go";
+import { Box, Typography, Divider, List, ListItem, ListItemButton, ListItemText, AppBar, Toolbar, IconButton, Button, Drawer, Menu, MenuItem, Badge } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export const Navbar = () => {
-  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const drawerWidth = 240;
+  const navItems = ['Home', 'About', 'Contact'];
+  const menuId = 'profile-menu-id'
 
-  const toggleProfileDropdown = () => {
-    setIsProfileOpen(!isProfileOpen);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <nav className="bg-white max-w-7xl mx-auto shadow-lg mt-5 rounded-lg sticky top-3">
-      <div className=" px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="flex-1 flex items-center md:justify-between sm:items-stretch sm:justify-start">
-            <div className="">
-              <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
-            </div>
-            <div className="hidden sm:block">
-              <div className="flex justify-center space-x-4">
-                <a
-                  href="#"
-                  className="text-gray-800 hover:text-gray-900 px-3 py-2 text-sm font-medium"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-800 hover:text-gray-900 px-3 py-2 text-sm font-medium"
-                >
-                  About
-                </a>
-              </div>
-            </div>
-            <div className="hidden sm:block ">
-              <div className="flex justify-center space-x-4">
-                <div className="flex">
-                  <a
-                    href="#"
-                    className="text-gray-800 hover:text-gray-900 px-1 py-2 text-xl font-medium"
-                  >
-                    <GoHeart />
-                  </a>
-                </div>
-                <div className="flex">
-                  <a
-                    href="#"
-                    className="text-gray-800 hover:text-gray-900 px-1 py-2 text-xl font-medium"
-                  >
-                    <TbGardenCart />
-                  </a>
-                </div>
-                <div  className="flex relative">
-                  <span
-                    className="text-gray-800 hover:text-gray-900 px-1 py-2 text-xl font-medium cursor-pointer"
-                    onClick={toggleProfileDropdown}
-                  >
-                    <CgProfile />
-                  </span>
-                  {isProfileOpen && <div className="absolute min-w-[200px] flex flex-col gap-3 p-4 bg-white shadow-lg top-[130%] rounded right-0">
-                    <a href="#"> Profile </a>
-                    <a href="#"> login </a>
-                  </div>}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <>
+      <AppBar component="nav" className="bg-white  sm:w-[80%] mx-auto sticky top-0 sm:mt-5 sm:rounded-lg shadow-lg">
+        <Toolbar className="justify-between">
+          <IconButton
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            className="text-gray-800"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} className="text-gray-700 font-semibold">
+                {item}
+              </Button>
+            ))}
+          </Box>
+
+          <Box>
+            <IconButton size="large" aria-label="show 4 new mails">
+              <Badge badgeContent={4} color="error">
+                <FavoriteBorderIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+            >
+              <Badge badgeContent={17} color="error">
+                <ShoppingBagIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      {renderMenu}
+    </>
   );
 };
