7ee00b9fe14097cfc01a7c56a32d0b93f3f2921b
patharharsh <patharharsh@gmail.com>
2024-03-19 09:56:26 +0530

    db file removed

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { dbConnect } from "@/app/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const con = await dbConnect();
-  return new NextResponse("connected to db");
-
-  //   return NextResponse.json({ messsage: "Hello World" });
-}
