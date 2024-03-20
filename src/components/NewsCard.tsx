45928d46f9a5b20557e4d3da42987fc334a2eabd
patharharsh <68531971+patharharsh@users.noreply.github.com>
2024-03-20 17:50:34 +0530

    Update NewsCard.tsx

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -5,7 +5,7 @@ const NewsCard = () => {
   return (
     <div className='hover:shadow-xl border border-1 rounded-xl group'>
       <div>
-        <img src="/bannar2.png" className='w-full' alt="news img" />
+        <img src="/Bannar2.png" className='w-full' alt="news img" />
       </div>
       <div className='p-5'>
         <h1 className='group-hover:text-[#00B207] text-xl mb-4'>Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.</h1>
