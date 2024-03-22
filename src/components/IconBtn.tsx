b11810ce87e1ef628292738b8c775ed8439e6f08
patharharsh <patharharsh@gmail.com>
2024-03-22 10:39:47 +0530

    Product page added

--- src/components/IconBtn.tsx
+++ src/components/IconBtn.tsx
@@ -1,19 +1,19 @@
 import classNames from 'classnames';
 import React from 'react'
 
-interface IconBtnProps {
+interface IconBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement>  {
     Icon: React.ElementType;
-    size: "small" | "large"
+    size?: "small" | "large"
 }
 
-const IconBtn = ({Icon, size}:IconBtnProps) => {
+const IconBtn = ({Icon, size = 'small', ...props}:IconBtnProps) => {
 
-   const BtnClasses = classNames({"text-black bg-gray-100 hover:bg-[#00B207] hover:text-[#ffffff] font-medium rounded-full text-sm p-5 text-center me-2 mb-2 " : size === "large"},
-   {"text-black bg-white hover:bg-[#00B207] hover:text-[#ffffff] font-medium rounded-full text-sm p-4 text-center me-2 mb-2 border border-1 " : size === "small"})
+   const BtnClasses = classNames({"text-black bg-gray-100 hover:bg-[#00B207] hover:text-[#ffffff] font-medium rounded-full text-sm p-5 text-center" : size === "large"},
+   {"text-black bg-white hover:bg-[#00B207] hover:text-[#ffffff] font-medium rounded-full text-sm p-4 text-center border border-1 " : size === "small"})
 
   return (
     <>
-        <button className={BtnClasses}><Icon size={size === "large" ? 28 : 22} /></button>
+        <button className={BtnClasses} {...props}><Icon size={size === "large" ? 28 : 22} /></button>
     </>
   )
 }
